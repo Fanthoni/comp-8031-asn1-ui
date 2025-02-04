ecbd0c7caccbd174301f33f8882f2966dff59f49
Sheng Qin <qinsheng32@gmail.com>
2025-02-03 17:37:54 -0800

    fix data loading issue for task details page

--- app/clients/taskDetails.tsx
+++ app/clients/taskDetails.tsx
@@ -9,12 +9,7 @@ export default function TaskDetailsScreen({ navigation }: { navigation: Navigati
 
   // Fetch task details using taskId
   // For demonstration, using a placeholder task
-  const [task, setTask] = useState({
-    id: "1",
-    task_type: "Vitals Check",
-    reminder_datetime: new Date(),
-    videoUrl: "https://www.youtube.com/watch?v=CKl8mhU_eR0",
-  });
+  const [task, setTask] = useState(JSON.parse(`{ "client_id": 3, "created_at": "Mon, 03 Feb 2025 08:36:29 GMT", "is_enabled": 1, "is_repetitive": 1, "reminder_datetime": "Mon, 03 Feb 2025 02:02:37 GMT", "reminder_id": 19, "repeat_pattern": null, "task_type": "House Keeping", "updated_at": "Mon, 03 Feb 2025 08:36:29 GMT" }`));
 
   useEffect(() => {
     console.log("Task ID:", taskId);
@@ -23,26 +18,32 @@ export default function TaskDetailsScreen({ navigation }: { navigation: Navigati
   }, [taskId]);
 
   const fetchTaskDetails = async (taskId: string) => {
-    fetch(`https://sheng.up.railway.app/api/reminders/${taskId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Task Details:", data);
-        setTask(data.reminder);
+    try {
+      fetch(`https://sheng.up.railway.app/api/reminders/${taskId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((error) => {
-        console.error("Error fetching task details:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Task Details:", data.reminder);
+          data.reminder.videoUrl = "https://www.youtube.com/watch?v=F9yF_mISzmI";
+          setTask(data.reminder);
+        })
+        .catch((error) => {
+          console.error("Error fetching task details:", error);
+        });
+    } catch (error) {
+      console.error("Error fetching task details:", error
+      );
+    }
   };
   return (
     <View style={styles.container} >
       <Text style={styles.header}>Task Details</Text>
       <Text style={styles.label}>Type: {task.task_type}</Text>
-      <Text style={styles.label}>Date: {task.reminder_datetime.toLocaleString()}</Text>
+      <Text style={styles.label}>Date: {task.reminder_datetime.toString()}</Text>
       {task.videoUrl && (
         <YoutubePlayer
           height={300}
